282c824e0cbe058aefba89df7d67e4550ea80013
Fredy Santamaria <77696721+fsantamaria1@users.noreply.github.com>
2024-03-11 22:50:26 -0400

    update: my journey card content with correct translations

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function changeLanguage() {
     fetch(`./translations/${selectedLanguage}.json`)
         .then(response => response.json())
         .then(data => {
-            // Update content with translations
+            // Update card titles with translations
             document.getElementById("welcome").innerText = data.welcome;
             document.getElementById("aboutMe").innerText = data.about_me;
             document.getElementById("myJourney").innerText = data.my_journey;
@@ -11,6 +11,8 @@ function changeLanguage() {
             document.getElementById("whatIKnow").innerText = data.what_i_know;
             document.getElementById("myLearningApproach").innerText = data.my_learning_approach;
             document.getElementById("myShortTermGoals").innerText = data.my_short_term_goals;
+            // update the card contents with translations
+            document.getElementById("myJourneyContent").innerHTML = data.my_journey_content;
         })
         .catch(error => console.error('Error fetching translations:', error));
 }
